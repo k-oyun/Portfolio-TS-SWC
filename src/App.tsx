47f8318ad76a9a2b0845d866bbb966fca107117c
k-oyun <113766051+k-oyun@users.noreply.github.com>
2025-02-13 20:19:00 +0900

    build: 프로젝트 세팅

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+function App() {
+  return (
+    <Router>
+      <Routes></Routes>
+    </Router>
+  );
+}
+
+export default App;
