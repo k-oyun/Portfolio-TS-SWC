84b6761d3cfc126556aa560995980bffc0bbb4ec
k-oyun <113766051+k-oyun@users.noreply.github.com>
2025-02-14 16:37:01 +0900

    chore: 프로젝트 세팅

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-
+import "./index.css";
+// vite는 main이 index.tsx역할
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
