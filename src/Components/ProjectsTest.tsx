35ab572fbf24b3177448d4a7ad5efe77dbc9ea98
k-oyun <113766051+k-oyun@users.noreply.github.com>
2025-02-26 20:50:42 +0900

    style: projectDetail 수정

--- src/Components/ProjectsTest.tsx
+++ src/Components/ProjectsTest.tsx
@@ -9,6 +9,8 @@ import mindBookList from "../Assets/Image/Png/mindBookList.png";
 import mindBookMain from "../Assets/Image/Png/mindBookMain.png";
 import mindBookReport from "../Assets/Image/Png/mindBookReport.png";
 import mindBookWriteReport from "../Assets/Image/Png/mindBookWriteReport.png";
+import mindBookModal1 from "../Assets/Image/Png/mindBookModal1.png";
+import mindBookModal2 from "../Assets/Image/Png/mindBookModal2.png";
 import waitherPc from "../Assets/Image/Png/waitherPc.png";
 import waitherMobile from "../Assets/Image/Png/waitherMobile.png";
 import waitherMorning from "../Assets/Image/Png/waitherMorning.png";
@@ -26,6 +28,10 @@ import deployHome from "../Assets/Image/Png/deployHome.png";
 import deployMain from "../Assets/Image/Png/deployMain.png";
 import deployMyPage from "../Assets/Image/Png/deployMyPage.png";
 import deployAddApp from "../Assets/Image/Png/deployAddApp.png";
+import deploRegister1 from "../Assets/Image/Png/deployRegister1.png";
+import deployRegister2 from "../Assets/Image/Png/deployRegister2.png";
+import deployEmailVerify from "../Assets/Image/Png/deployEmailVerify.png";
+import deployResetPassword from "../Assets/Image/Png/deployResetPassword.png";
 import { useMediaQuery } from "react-responsive";
 import { motion } from "framer-motion";
 
@@ -173,8 +179,10 @@ const ProjectCard = styled.div<styleType>`
 const ProjectTitle = styled.span<styleType>`
   padding-bottom: ${(props) => (props.$ismobile ? "4px" : "30px")};
   font-size: ${(props) => (props.$ismobile ? "20px" : "40px")};
-  text-shadow: 0 0 1px, 0 0 7px #00ff00, 0 0 10px #00ff00, 0 0 5px #00ff00,
-    0 0 33px #00ff00, 0 0 0px #00ff00, 0 0 0px #00ff00;
+  /* text-shadow: 0 0 1px, 0 0 7px #00ff00, 0 0 10px #00ff00, 0 0 5px #00ff00,
+    0 0 33px #00ff00, 0 0 0px #00ff00, 0 0 0px #00ff00; */
+  text-shadow: 0 0 1px, 0 0 7px blue, 0 0 10px blue, 0 0 5px blue, 0 0 33px blue,
+    0 0 0px blue, 0 0 0px blue;
 `;
 
 const SubTitle = styled.span<styleType>`
@@ -359,7 +367,7 @@ const ModalCloseBtn = styled.button<styleType>`
 
 const ModalMainTitle = styled.span<styleType>`
   font-size: ${(props) => (props.$ismobile ? "15px" : "30px")};
-  margin: ${(props) => (props.$ismobile ? "5px 0" : "20px 0")};
+  margin: ${(props) => (props.$ismobile ? "10px 0" : "20px 0")};
   text-shadow: 0 0 1px, 0 0 7px #00ff00, 0 0 10px #00ff00, 0 0 5px #00ff00,
     0 0 33px #00ff00, 0 0 0px #00ff00, 0 0 0px #00ff00;
 `;
@@ -368,7 +376,7 @@ const ModalContent = styled.span<styleType>`
   text-shadow: 0 0 1px, 0 0 7px #f148fb, 0 0 10px #f148fb, 0 0 5px #f148fb,
     0 0 33px #f148fb, 0 0 0px #f148fb, 0 0 0px #f148fb;
   white-space: pre-wrap;
-  line-height: 30px;
+  line-height: ${(props) => (props.$ismobile ? "13px" : " 30px")};
 `;
 
 const ModalLinkContent = styled.span<styleType>`
@@ -455,8 +463,8 @@ const Projects = forwardRef<HTMLDivElement, any>((props, ref) => {
       id: 3,
       title: "MindBook",
       introduce: isMobile
-        ? "우울감, 무기력함, 불안감, 의욕 상실을 겪는\n현대인들을 위해 자기 치유와\n자존감 회복을 목적으로 제공된 웹서비스"
-        : "우울감, 무기력함, 불안감, 의욕 상실을 겪는 현대인들을 위해 자기 치유와 자존감 회복을 목적으로 제공된 웹서비스",
+        ? "우울감, 무기력함, 불안감, 의욕 상실을 겪는\n현대인들을 위해 자기 치유와\n자존감 회복을 목적으로 제공된 웹 서비스"
+        : "우울감, 무기력함, 불안감, 의욕 상실을 겪는 현대인들을 위해 자기 치유와 자존감 회복을 목적으로 제공된 웹 서비스",
       period: "2024.07 ~ 2024.08",
       tech: isMobile ? "HTML\nCSS\nJavaScript" : "HTML, CSS, JavaScript",
       image: isMobile ? mindBookMobile : mindBookPc,
@@ -485,10 +493,13 @@ const Projects = forwardRef<HTMLDivElement, any>((props, ref) => {
         waitherWindSetting,
       ],
       part: [
-        "[ 프론트엔드 ]",
+        "[ 프론트엔드, 디자인 ]",
         "ㆍ Splash Screen, 아이콘 설정 등 앱 초기 환경 설정",
-        "ㆍ 회원가입, 로그인, 비밀번호 재설정, 직장 지역 / 알림 / 사용자 데이터 / 메인화면 날씨 정보 설정화면, 메인화면, 레포트 화면 구현 및 API 연결",
+        isMobile
+          ? "ㆍ 회원가입, 로그인, 비밀번호 재설정, 직장 지역 / 알림 / 사용자 데이터 / \n    메인화면 날씨 정보 설정화면, 메인화면, 레포트 화면 구현 및 API 연결"
+          : "ㆍ 회원가입, 로그인, 비밀번호 재설정, 직장 지역 / 알림 / 사용자 데이터 / 메인화면 날씨 정보 설정, 메인, 레포트 화면 구현 및 API 연결",
         "ㆍ 바람세기 설정, 개인정보 설정 API 연결",
+        "ㆍ 레포트 화면, 사용자 설정 화면 디자인",
         "ㆍ 카카오 로그인 구현",
         "ㆍ 서버로부터 전달받은 데이터를 적절히 처리하는 정제 함수 모듈화",
         "ㆍ API 호출 코드 모듈화를 통한 코드 단순화",
@@ -496,44 +507,95 @@ const Projects = forwardRef<HTMLDivElement, any>((props, ref) => {
       ],
       task: [
         "ㆍ react-native-modal을 이용하여 모달 구현",
-        "ㆍ react-native-keyboard-aware-scroll-view를 이용하여 키보드가 입력 칸을 가리는 경우 스크롤이 가능하도록 구현",
+        isMobile
+          ? "ㆍ react-native-keyboard-aware-scroll-view를 이용하여 키보드가 입력 칸을 \n    가리는 경우 스크롤이 가능하도록 구현"
+          : "ㆍ react-native-keyboard-aware-scroll-view를 이용하여 키보드가 입력 칸을 가리는 경우 스크롤이 가능하도록 구현",
         "ㆍ react-navigation을 이용하여 화면 전환 기능 구현",
-        "ㆍ gorhom/bottom-sheet, react-native-picker/picker를 이용하여 시간을 선택할 수 있는 바텀 시트 구현",
+        isMobile
+          ? "ㆍ gorhom/bottom-sheet, react-native-picker/picker를 이용하여 \n    시간을 선택할 수 있는 바텀 시트 구현"
+          : "ㆍ gorhom/bottom-sheet, react-native-picker/picker를 이용하여 시간을 선택할 수 있는 바텀 시트 구현",
         "ㆍ 사용자 설정 토글 구현",
-        "ㆍ react-native-community/slider를 이용하여 사용자 민감도 조절이 가능한 슬라이딩 바 구현",
+        isMobile
+          ? "ㆍ react-native-community/slider를 이용하여 사용자 민감도 조절이\n    가능한 슬라이딩 바 구현"
+          : "ㆍ react-native-community/slider를 이용하여 사용자 민감도 조절이 가능한 슬라이딩 바 구현",
         "ㆍ react-native-seoul/kakao-login을 이용하여 카카오 로그인 구현",
         "ㆍ 카카오 맵 API를 이용하여 위치 검색 기능 구현",
-        "ㆍ react-native-background-fetch, react-native-geolocation을 이용하여 위치를 15분마다 전송하도록 구현",
-        "ㆍ react-native-camera-roll, react-native-view-shot을 이용하여 하루 날씨 이슈가 정리된 화면을 갤러리에 저장할 수 있는 기능 구현",
-        "ㆍ react-native의 RefreshControl을 이용하여 화면 드래그를 통한 새로고침 기능 구현",
+        isMobile
+          ? "ㆍ react-native-background-fetch, react-native-geolocation을 이용하여\n    위치를 15분마다 전송하도록 구현"
+          : "ㆍ react-native-background-fetch, react-native-geolocation을 이용하여 위치를 15분마다 전송하도록 구현",
+        isMobile
+          ? "ㆍ react-native-camera-roll, react-native-view-shot을 이용하여 하루 날씨 \n    이슈가 정리된 화면을 갤러리에 저장할 수 있는 기능 구현"
+          : "ㆍ react-native-camera-roll, react-native-view-shot을 이용하여 하루 날씨 이슈가 정리된 화면을 갤러리에 저장할 수 있는 기능 구현",
+        isMobile
+          ? "ㆍ react-native의 RefreshControl을 이용하여 화면 드래그를 통한 \n    새로고침 기능 구현"
+          : "ㆍ react-native의 RefreshControl을 이용하여 화면 드래그를 통한 새로고침 기능 구현",
       ],
       trouble: [
-        "ㆍ JavaScript fetch 함수를 이용한 API 호출 시 컴포넌트가 렌더링 되는 모든 경우에 API 호출 발생",
+        isMobile
+          ? "ㆍ JavaScript fetch 함수를 이용한 API 호출 시 컴포넌트가 렌더링 되는\n    모든 경우에 API 호출 발생"
+          : "ㆍ JavaScript fetch 함수를 이용한 API 호출 시 컴포넌트가 렌더링 되는 모든 경우에 API 호출 발생",
         "  ㆍ react-query를 이용하여 수신한 데이터를 10분 동안 캐싱하도록 구현",
         "  ㆍ API 호출 수 감소 및 효율성 개선",
         "\n",
-        "ㆍ background 기능 구현 도중  background-fetch 정상 작동 여부 확인이 불가능한 문제 발생",
+        isMobile
+          ? "ㆍ background 기능 구현 도중  background-fetch 작동 여부\n   확인이 불가능한 문제 발생"
+          : "ㆍ background 기능 구현 도중  background-fetch 작동 여부 확인이 불가능한 문제 발생",
         "  ㆍ appstate를 이용하여 background-fetch 3가지 상태를 확인",
         "\n",
-        "ㆍ react-query를 이용한 api 호출 과정에서 데이터 전송이 끝나지 않은 상태에서 화면이 렌더링 되는 문제 발생",
-        "   ㆍ 데이터가 준비될 때까지 컴포넌트 렌더링을 중지시키는 useSuspenseQuery 훅 이용",
+        isMobile
+          ? "ㆍ react-query를 이용한 api 호출 과정에서 데이터 로드가 끝나지 않은\n    상태에서 컴포넌트가 렌더링 되는 문제 발생"
+          : "ㆍ react-query를 이용한 api 호출 과정에서 데이터 로드가 끝나지 않은 상태에서 컴포넌트가 렌더링 되는 문제 발생",
+
+        "   ㆍ useSuspenseQuery 훅을 사용해 데이터 로드 전까지 컴포넌트 렌더링 제어",
+
         "\n",
-        "ㆍ react-native-camera-roll을 이용하여 사진 저장 기능 구현 도중 null 값이 prop으로 전달되어 발생하는 NSNumber 에러 발생",
-        "   ㆍ 해당 prop에 null 값이 전달되지 않도록 보장하여 NSNumber 에러가 발생하지 않도록 내장 함수 수정",
+        isMobile
+          ? "ㆍ react-native-camera-roll을 이용하여 사진 저장 기능 구현 도중 null 값이\n    prop으로 전달되어 발생하는 NSNumber 에러 발생"
+          : "ㆍ react-native-camera-roll을 이용하여 사진 저장 기능 구현 도중 null 값이 prop으로 전달되어 발생하는 NSNumber 에러 발생",
+        isMobile
+          ? "   ㆍ 해당 prop에 null 값이 전달되지 않도록 보장하여\n       NSNumber 에러가 발생하지 않도록 내장 함수 수정"
+          : "   ㆍ 해당 prop에 null 값이 전달되지 않도록 보장하여 NSNumber 에러가 발생하지 않도록 내장 함수 수정",
       ],
       feeling: [
-        "ㆍ 오래 알고 지냈던 친구들과 진행했던 프로젝트였기에 갈등 없이 프로젝트를 끝낼 수 있다고 생각했습니다. \n   프로젝트 진행 도중 팀원 간 의견 차이로 갈등이 생기기 시작했고, 프로젝트가 중단되었습니다. \n   정이 많았던 프로젝트였기에 끝까지 구현하고 싶어 남아있는 부분들을 구현했습니다. \n   힘들었지만 의사소통과 팀원 간의 존중 및 배려가 중요하다고 한 번 더 배워갈 수 있었던 프로젝트였습니다.",
+        isMobile
+          ? "ㆍ 오래 알고 지냈던 친구들과 진행했던 프로젝트였기에 갈등 없이 프로젝트를 끝\n    낼 수 있다고 생각했습니다. 프로젝트 진행 도중 팀원 간 의견 차이로 갈등이 생\n    기기 시작했고, 프로젝트가 중단되었습니다. 정이 많았던 프로젝트였기에 끝까\n    지 구현하고 싶어 남아있는 부분들을 구현했습니다.  힘들었지만 의사소통과 \n    팀원 간의 존중 및 배려가 중요하다고  한 번 더 배워갈 수 있었던 프로젝트\n    였습니다."
+          : "ㆍ 오래 알고 지냈던 친구들과 진행했던 프로젝트였기에 갈등 없이 프로젝트를 끝낼 수 있다고 생각했습니다. \n    프로젝트 진행 도중 팀원 간 의견 차이로 갈등이 생기기 시작했고, 프로젝트가 중단되었습니다.\n    정이 많았던 프로젝트였기에 끝까지 구현하고 싶어 남아있는 부분들을 구현했습니다.\n    힘들었지만 의사소통과 팀원 간의 존중 및 배려가 중요하다고 한 번 더 배워갈 수 있었던 프로젝트였습니다.",
         "\n",
-        "ㆍ 카카오 로그인 및 이미지 갤러리 저장과 같은 기능을 구현하며, OS의 요구 사항에 맞춰 개발하는 과정에서 많은 시행착오와 학습이 필요했습니다.\n   플랫폼별 제약 사항을 고려하여 구현하는 데 어려움을 겪었지만, OS의 세부적인 부분까지 신중하게 접근하는 것이 중요하다는 것을 깨달았습니다.",
+        isMobile
+          ? "ㆍ 카카오 로그인 및 이미지 갤러리 저장과 같은 기능을 구현하며, OS의 요구 사\n    항에 맞춰 구현하는 과정에서 많은 시행착오와 학습이 필요했습니다. 플랫폼별 \n    제약 사항을 고려하여 구현하는 데 어려움을 겪었지만, OS의 세부적인 부분까\n    지 신중하게 접근하는 것이 중요하다는 것을 깨달았습니다."
+          : "ㆍ 카카오 로그인 및 이미지 갤러리 저장과 같은 기능을 구현하며, OS의 요구 사항에 맞춰 구현하는 과정에서 많은 시행착오와 학습이 필요했습니다.\n    플랫폼별 제약 사항을 고려하여 구현하는 데 어려움을 겪었지만, OS의 세부적인 부분까지 신중하게 접근하는 것이 중요하다는 것을 깨달았습니다.",
       ],
       link: "https://github.com/k-oyun/Waither-FE",
     },
     {
-      image: [deployLogin, deployHome, deployMain, deployMyPage, deployAddApp],
-      part: [],
-      task: ["파일 업로드 기능 구현", "헤더 구현", "드롭다운 기능 구현"],
-      trouble: [],
-      feeling: [],
+      image: [
+        deployLogin,
+        deploRegister1,
+        deployRegister2,
+        deployEmailVerify,
+        deployHome,
+        deployMain,
+        deployMyPage,
+        deployAddApp,
+
+        deployResetPassword,
+      ],
+      part: [
+        "[ 프론트엔드, 디자인 ]",
+        "ㆍ 각 페이지 구현 및 API 연결",
+        "ㆍ 카카오 로그인 구현",
+        "ㆍ 토큰 암호화 및 복호화",
+      ],
+      task: [
+        "ㆍ 파일 업로드 기능 구현",
+        "ㆍ Header 구현",
+        "ㆍ Footer 구현",
+        "ㆍ 이메일 인증, 비밀번호 재설정 모달 구현",
+        "ㆍ 비밀번호, 토큰 등 민감한 정보 보기/숨기기 기능 구현",
+        "ㆍ react-router-dom을 이용하여 SPA 내부에서 페이지 이동이 가능하도록 구현",
+      ],
+      trouble: [""],
+      feeling: [""],
       link: "https://github.com/k-oyun/Deploy-Surfing",
     },
     {
@@ -543,11 +605,25 @@ const Projects = forwardRef<HTMLDivElement, any>((props, ref) => {
         mindBookList,
         mindBookReport,
         mindBookWriteReport,
+        mindBookModal1,
+        mindBookModal2,
       ],
-      part: [],
-      task: ["각 데이터 정제함수 구현", "", ""],
-      trouble: [],
-      feeling: [],
+      part: [
+        "[ 프론트엔드, 기획 ]",
+        "ㆍ 메인, 전체 책장, 도서 검색, 도서 추가, 도서 독후감 작성 페이지 구현 및 API 연결",
+      ],
+      task: ["ㆍ 데이터 정제함수 구현", "ㆍ 도서 추가 시 상태 모달 구현", ""],
+      trouble: [
+        "ㆍ API 연결 중 서버에서의 CORS 에러 발생으로 인한 요청 차단",
+        isMobile
+          ? "   ㆍ Server측에 Access-Control-Allow-Origin 헤더의 리스트에 프론트엔드 \n       개발 서버 URL을 추가하여 해결"
+          : "   ㆍ Server측에 Access-Control-Allow-Origin 헤더의 리스트에 프론트엔드 개발 서버 URL을 추가하여 해결",
+      ],
+      feeling: [
+        isMobile
+          ? "ㆍ 데드라인이 짧았던 해커톤 프로젝트였기에 빠른 의사소통과 효과적인 협업이\n    중요했습니다. 구현 과정에서 서버 측 문제를 신속하게 공유하며 해결해 나갔\n    음에도 불구하고 마감 1시간 전, 배포 과정에서 문제가 발생했습니다. 조급해진\n    팀원들 사이에 긴장감이 감돌았고, 날이 선 말들이 오가며 분위기가 얼어붙었\n    습니다. 마감이 끝난 후, 적막 속에서 서로를 위로하며 프로젝트를 마무리할 수\n    있었습니다. 이 프로젝트를 통해, 완벽한 협업을 목표로 하더라도 갈등은 피할\n    수 없다는 것, 그리고 진행 과정에서 감정이 요동치는 것은 자연스러운 일이지\n    만, 어떻게 해결하느냐가 바로 팀워크라는 것을 배웠습니다."
+          : "ㆍ 데드라인이 짧았던 해커톤 프로젝트였기에 빠른 의사소통과 효과적인 협업이 중요했습니다. 구현 과정에서 서버 측 문제를 신속하게 공유하며 해결해\n    나갔음에도 불구하고 마감 1시간 전, 배포 과정에서 문제가 발생했습니다. 조급해진 팀원들 사이에 긴장감이 감돌았고, 날이 선 말들이 오가며 분위기가\n    얼어붙었습니다. 마감이 끝난 후, 적막 속에서 서로를 위로하며 프로젝트를 마무리할 수 있었습니다. 이 프로젝트를 통해, 완벽한 협업을 목표로 하더라\n    도 갈등은 피할 수 없다는 것, 그리고 진행 과정에서 감정이 요동치는 것은 자연스러운 일이지만, 어떻게 해결하느냐가 바로 팀워크라는 것을 배웠습니\n    다.",
+      ],
       link: "https://github.com/k-oyun/MindBook-FE",
     },
   ];
