586c3c480064b7bdbda7a195dc493d5544d9952e
k-oyun <113766051+k-oyun@users.noreply.github.com>
2025-02-27 20:05:44 +0900

    design: padding 수정

--- src/Components/Projects.tsx
+++ src/Components/Projects.tsx
@@ -208,7 +208,7 @@ const ContentTxt = styled.span<styleType>`
 `;
 
 const DetailTxt = styled.span<styleType>`
-  padding-top: ${(props) => (props.$ismobile ? "40px" : "35px")};
+  padding-top: ${(props) => (props.$ismobile ? "40px" : "2.15rem")};
   padding-left: ${(props) => (props.$ismobile ? "220px" : "0px")};
   font-size: ${(props) => (props.$ismobile ? "15px" : "25px")};
   text-shadow: 0 0 1px, 0 0 7px red, 0 0 10px red, 0 0 5px red, 0 0 33px red,
